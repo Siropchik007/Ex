94b30e04e68d690f3b6f6513b5a5f4b18429177f
Даниил <kourin.daniil@gmail.com>
2022-04-26 09:22:08 +0300

    Ex

---
+++ Exam/TaskList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public class TaskList
+    {
+
+        /// <summary>
+        /// name - имя списка с задачами.
+        ///tasks - список задач, относящихся к списку
+        /// </summary>
+        private string Name;
+        private List<Task> tasks;
+
+        /// <summary>
+        /// Конструктор инициализирует список задач с указанным именем
+        /// </summary>
+        /// <param name="name"></param>
+        public TaskList(string name)
+        {
+            Name = name;
+            tasks = new List<Task>();
+        }
+
+        /// <summary>
+        /// GetName - возвращает имя списка задач
+        /// </summary>
+        /// <returns></returns>
+        public string GetName()
+        {
+            return Name;
+        }
+
+        /// <summary>
+        /// AddTask - добавляет задачу в список задач
+        /// </summary>
+        /// <param name="task"></param>
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+
+
+        /// <summary>
+        /// GetTasks - возвращает список всех задач
+        /// </summary>
+        /// <returns></returns>
+
+        public List<Task> GetTasks()
+        {
+            return tasks;
+        }
+
+        /// <summary>
+        /// Remove - удаляет задачу из списка задач
+        /// </summary>
+        /// <param name="task"></param>
+
+        public void Remove (Task task)
+        {
+            tasks.Remove(task);
+        }
+
+        /// <summary>
+        /// GetTasksByToday - возвращает задачи запланированные на сегодня в хронологическом порядке.
+        /// </summary>
+        /// <returns></returns>
+
+        public List<Task> GetTasksByToday()
+        {
+            return tasks.Where(x => x.Due == DateTime.Now).ToList();
+        }
+
+        /// <summary>
+        /// GetTasksByFuture - возвращает задачи на завтра и позже в хронологическом порядке.
+        /// </summary>
+        /// <returns></returns>
+
+
+        public List<Task> GetTasksByFuture()
+        {
+            return tasks.Where(x => x.Due > DateTime.Now).ToList();
+        }
+
+    }
+}
